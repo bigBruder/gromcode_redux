a149d71d7d441cbc684703a9a82647d3b84dfedb
romontherock <romontherock@example.com>
2022-09-18 16:29:13 +0300

    redux

--- Lesson2IntroToRedux/task2/src/index.js
+++ Lesson2IntroToRedux/task2/src/index.js
@@ -1,26 +1,30 @@
-import {createStore} from 'redux';
+import './index.scss';
+import store, {increment, decrement, reset} from './store';
 
-const INCREMENT = 'COUNTER/INCREMENT';
-const increment = () =>{
-    return {
-        type: INCREMENT,
-    };
-};
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
 
-const DECREMENT = 'COUNTER/DECREMENT';
-const decrement = () => {
-    return{
-        type: DECREMENT,
-    };
-};
-
-const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return state + 1;
-        case DECREMENT:
-            return state - 1;
-            default: 
-            return state;
-    }
+const onIncrement = () =>{
+    store.dispatch(increment());
 }
+const onDecrement = () =>{
+    store.dispatch(decrement());
+}
+const onReset = () =>{
+    store.dispatch(reset());
+}
+
+incrementBtn.addEventListener('click', onIncrement);
+decrementBtn.addEventListener('click', onDecrement);
+resetBtn.addEventListener('click', onReset);
+
+store.subscribe(() => {
+const state = store.getState();
+const currentValue = state.history.reduce((acc, value) => acc + +value, 0 )
+const historyString = state.history.join('');
+resultElem.textContent = state.history.length === 0 
+? ''
+: `${historyString} = ${currentValue}`;
+}) 
