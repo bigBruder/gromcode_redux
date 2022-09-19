d8d0c7fea9ea49448d99a53fbb3200b07f5c1e55
romontherock <romontherock@example.com>
2022-09-19 19:25:46 +0300

    redux

---
+++ Lesson3CombiningReducers/task2/src/counter.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const DECREMENT = 'COUNTER/DECREMENT';
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+export const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(
+  counterReducer,
+  +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
