3f96d84ce950086fa82c4b5657e71696ff2b98df
romontherock <romontherock@example.com>
2022-09-19 19:00:48 +0300

    redux

--- Lesson3CombiningReducers/homeTask/src/users.actions.js
+++ Lesson3CombiningReducers/homeTask/src/users.actions.js
@@ -2,30 +2,24 @@ export const ADD = 'USER/ADD';
 export const DELETE = 'USER/DELETE';
 export const UPDATE = 'USER/UPDATE';
 
-export const addUser = userData => {
-  return {
-    type: ADD,
-    payload: {
-      userData,
-    },
-  };
-};
+export const addUser = userData => ({
+  type: ADD,
+  payload: {
+    userData,
+  },
+});
 
-export const deleteUser = id => {
-  return {
-    type: DELETE,
-    payload: {
-      userId,
-    },
-  };
-};
+export const deleteUser = userId => ({
+  type: DELETE,
+  payload: {
+    userId,
+  },
+});
 
-export const updateUser = (id, userData) => {
-  return {
-    type: DELETE,
-    payload: {
-      userId,
-      userData,
-    },
-  };
-};
+export const updateUser = (userId, userData) => ({
+  type: DELETE,
+  payload: {
+    userId,
+    userData,
+  },
+});
