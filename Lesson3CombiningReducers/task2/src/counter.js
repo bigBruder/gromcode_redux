854f889290966833ef9a3b072710a5b3848724d2
romontherock <romontherock@example.com>
2022-09-19 19:29:08 +0300

    redux

--- Lesson3CombiningReducers/task2/src/counter.js
+++ Lesson3CombiningReducers/task2/src/counter.js
@@ -23,7 +23,7 @@ export const counterReducer = (state = 0, action) => {
 
 export const store = createStore(
   counterReducer,
-  +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 store.dispatch(increment());
