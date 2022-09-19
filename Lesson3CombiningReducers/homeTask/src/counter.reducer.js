6a8f78593519b960549ab1ecbefea69735263d73
romontherock <romontherock@example.com>
2022-09-19 18:41:14 +0300

    redux

--- Lesson3CombiningReducers/homeTask/src/counter.reducer.js
+++ Lesson3CombiningReducers/homeTask/src/counter.reducer.js
@@ -1,26 +1,13 @@
 import { INCREMENT, DECREMENT, RESET } from './counter.actions';
 
-const initialState = {
-  history: [],
-};
-
-const counterReducer = (state = initialState, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return {
-        ...state,
-        history: state.history.concat(`+${1}`),
-      };
+      return state + 1;
     case DECREMENT:
-      return {
-        ...state,
-        history: state.history.concat(-1),
-      };
+      return state - 1;
     case RESET:
-      return {
-        ...state,
-        history: [],
-      };
+      return 0;
     default:
       return state;
   }
--- Lesson3CombiningReducers/homeTask/src/index.js
+++ Lesson3CombiningReducers/homeTask/src/index.js
@@ -3,6 +3,7 @@ import { addUser, deleteUser } from './users.actions';
 import { increment, decrement, reset } from './counter.actions';
 
 store.dispatch(addUser({ name: 'Roman', id: 1 }));
+store.dispatch(addUser({ name: 'Roman', id: 2 }));
 store.dispatch(deleteUser(1));
 store.dispatch(increment());
 store.dispatch(reset());
