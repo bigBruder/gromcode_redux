db63ee23a6d0d287003cfd7d092422501e3a32ed
romontherock <romontherock@example.com>
2022-09-29 01:27:44 +0300

    reduxSelectors

--- Lesson5Selectors/task2/src/store.js
+++ Lesson5Selectors/task2/src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
-import usersPageReducer from './users/users.reducer';
+import optionsReducer from './options/options.reducer';
 
 const store = createStore(
-  usersPageReducer,
+  optionsReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 export default store;
