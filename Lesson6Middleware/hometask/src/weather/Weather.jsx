9487e062acad75b9c4d33540a17de30180326590
romontherock <romontherock@example.com>
2022-09-30 19:17:38 +0300

    reduxMiddleware

--- Lesson6Middleware/hometask/src/weather/Weather.jsx
+++ Lesson6Middleware/hometask/src/weather/Weather.jsx
@@ -12,8 +12,8 @@ class Weather extends Component {
         <h1 className="weather__title">Weather data</h1>
         <ul className="cities-list">
           {this.props.weatherData.map(weatherData => (
-            <li key={weatherData.name} className="city">
-              <span className="city__name">{weatherData.city}</span>
+            <li key={weatherData.id} className="city">
+              <span className="city__name">{weatherData.name}</span>
               <span className="city__temperature">{weatherData.temperature}</span>
             </li>
           ))}
--- Lesson6Middleware/hometask/src/weather/weather.gateway.js
+++ Lesson6Middleware/hometask/src/weather/weather.gateway.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://5e5cf5eb97d2ea001479f01.mockapi.io/api/v1/cities';
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities';
 
 export const getData = () =>
   fetch(baseUrl).then(response => {
